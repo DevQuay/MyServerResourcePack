68afc29ba2ba87f066f233f6b8217fab7b8c4094
DevQuay <josh.childe@gmail.com>
2020-04-13 14:40:55 +0100

    Server Started, Updating SHA1

--- TestScript.sh
+++ TestScript.sh
@@ -1,4 +1,7 @@
+7z u ./CustomPaintings.zip ./CustomPaintings/*
 git commit -a -m "Server Started, Updating SHA1"
 git push
-7z u ./CustomPaintings.zip ./CustomPaintings/*              
-sudo sed -i.bak "s/^resource-pack-sha1=(?:.*)$/resource-pack-sha1=$(sha1sum ./CustomPaintings.zip|cut -f1 -d' ')/" /srv/forge-1.14.4/server.properties
+newhash=$(sha1sum ./CustomPaintings.zip|cut -f1 -d" ")
+sudo sed -i.bake 's/resource-pack-sha1=.*/resource-pack-sha1='"$newhash"'/' /srv/forge-1.14.4/server.properties >/home/floyd/Desktop/servar.txt
+forge-1.14.4d start
+forge-1.14.4d console
